fa713a0a3e1f132f8221fcddaa54709b16ad2c3a
CristinaR08 <cris.04082009@hotmail.com>
2023-06-26 19:44:05 -0500

    NamedQuery

---
+++ src/main/java/com/example/demo/service/CalculoCuenta.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import java.math.BigDecimal;
+
+public interface CalculoCuenta {
+	
+	public BigDecimal  valorMatricula(BigDecimal montoCuenta);
+
+}
---
+++ src/main/java/com/example/demo/service/IEstudianteService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+public interface IEstudianteService {
+	
+	public void agregar(Estudiante estudiante);
+	public void modificar(Estudiante estudiante);
+	public Estudiante encontrar(String cedula);
+	public void borrar(String cedula);
+	public Estudiante encontrarPorApellido(String apellido);
+	public Estudiante encontrarPorApellidoTyped(String apellido);
+	public List<Estudiante> encontrarListaPorApellido(String apellido);
+	public Estudiante encontrarPorApellidoyNombre(String apellido, String nombre);
+}
